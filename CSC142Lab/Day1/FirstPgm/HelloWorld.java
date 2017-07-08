cb74aa7a81b97a038c4edccd49df09e884e74abb
kevin zhang <zhangzheng888@gmail.com>
2017-07-07 18:08:23 -0700

    added folder for class

---
+++ CSC142Lab/Day1/FirstPgm/HelloWorld.java
@@ -0,0 +1,37 @@
+
+// instructor preferred convention
+
+public class HelloWorld {
+
+        public static void main(String[ ] args) {
+            System.out.println("Hello World!");
+            // string literals have to be enclosed by quotation marks
+            
+            // ends with semi-colon
+            
+           
+        
+        }
+        // auto indent with ctrl + shift + i
+        
+        // f4 deletes a whole line
+        
+        // ctrl + k to compile
+        
+}
+
+/* paragraphs of comments
+ * 
+ * escape characters
+ * 
+ * initiate with "\"
+ * 
+ * new line "\n"
+ * 
+ * tab "\t"
+ * 
+ * backslash "\\"
+ * 
+ * runtime error - error that halts the program, "blown up"/"explosivo"
+ * 
+ */
